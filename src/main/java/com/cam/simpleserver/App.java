1a07fba1849abd22ae3e0c99b176c4121186f01b
cwilson <campbellwilson@yahoo.com>
2011-12-10 15:39:02 +0000

    Importing initial simple-server project

---
+++ src/main/java/com/cam/simpleserver/App.java
@@ -0,0 +1,13 @@
+package com.cam.simpleserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
